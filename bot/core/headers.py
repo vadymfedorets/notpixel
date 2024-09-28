dd78fda8ddfb404a579691d286e0830ed778eceb
Alex <fedorets.vadim@gmail.com>
2024-09-28 14:50:53 +0300

    [functionality] squads&new tasks

--- bot/core/headers.py
+++ bot/core/headers.py
@@ -13,3 +13,18 @@ headers = {
     'Sec-Ch-Ua-platform': '"Android"',
     'User-Agent': 'Mozilla/5.0 (Linux; Android 14) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.6422.165 Mobile Safari/537.36'
 }
+
+headers_squads = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:130.0) Gecko/20100101 Firefox/130.0",
+    "Accept": "*/*",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Accept-Encoding": "gzip, deflate, br, zstd",
+    "Referer": "https://webapp.notcoin.tg/",
+    "content-type": "application/json",
+    "Origin": "https://webapp.notcoin.tg",
+    "Connection": "keep-alive",
+    "Sec-Fetch-Dest": "empty",
+    "Sec-Fetch-Mode": "cors",
+    "Sec-Fetch-Site": "same-site",
+    "Priority": "u=4",
+}
